ea593b9317a6444ba7d723d20db4fa7c5d247d25
Dragan Ćulibrk <draganculibrk9@uns.ac.rs>
2021-07-07 21:09:03 +0200

    Update prometheus_handler.go

--- src/utils/prometheus_handler/prometheus_handler.go
+++ src/utils/prometheus_handler/prometheus_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"strconv"
+	"time"
 )
 
 func GetHttpRequestsCounter() *prometheus.CounterVec {
@@ -26,15 +27,28 @@ func GetHttpRequestsSize() prometheus.Counter {
 	)
 }
 
-func PrometheusMiddleware(requestsCounter *prometheus.CounterVec, requestsSize prometheus.Counter) gin.HandlerFunc {
+func GetUniqueClients() *prometheus.CounterVec {
+	return prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "unique_users_count",
+			Help: "Number of unique users",
+		},
+		[]string{"IP", "timestamp", "browser"},
+	)
+}
+
+func PrometheusMiddleware(requestsCounter *prometheus.CounterVec, requestsSize prometheus.Counter, uniqueClients *prometheus.CounterVec) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requestSize := int(c.Request.ContentLength)
+		browser := c.Request.UserAgent()
+		ip := c.GetHeader("X-Forwarded-For")
 
 		c.Next()
 
 		responseSize := c.Writer.Size()
-		requestsCounter.WithLabelValues(c.Request.Method, c.Request.RequestURI, strconv.Itoa(c.Writer.Status()))
+		requestsCounter.WithLabelValues(c.Request.Method, c.Request.RequestURI, strconv.Itoa(c.Writer.Status())).Inc()
 		requestsSize.Add(float64((requestSize + responseSize) / 1024 / 1024))
+		uniqueClients.WithLabelValues(ip, time.Now().Format(time.UnixDate), browser).Inc()
 	}
 }
 
